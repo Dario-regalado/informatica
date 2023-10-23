dff940a72e8a9335ed120d350b7d77bb61619524
Darío <alu0101640150@ull.edu.es>
2023-10-23 20:38:57 +0100

    prueba

--- jutge/alternatives-iterations/general.cc
+++ jutge/alternatives-iterations/general.cc
@@ -2,13 +2,17 @@
 #include <iomanip>
 
 int main() {
-    int numero1{0};
-    double suma_encadenada{0.0};
+    int primer_año{0};
+    int segundo_año{0};
 
-    std::cin >> numero1;
-    for (int i = 1; i <= numero1; i++){
-        suma_encadenada += (1.0 / i); 
+    std::cin >> primer_año >> segundo_año;
+    if (primer_año > segundo_año){
+        return 1;
+        if (primer_año < 1800 && segundo_año > 9999){
+            return 2;
+        }
+        
     }
-    std::cout << std::fixed << std::setprecision(4) << suma_encadenada << std::endl;
+    
     return 0;
 }
