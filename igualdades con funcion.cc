af788343cd35378b1c604da4c49b85294e0e684e
Darío <alu0101640150@ull.edu.es>
2023-10-25 19:22:03 +0100

    todo

---
+++ igualdades con funcion.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void comprobacion(int condicion1, int condicion2, std::string& result) {
+    if (condicion1 == condicion2) {
+        result = "true";
+    } else{
+        result = "false";
+    }
+    return;
+}
+
+int main() {
+   int valor1{0};
+   int valor2{0}; 
+   
+   std::cout << "introduzca dos valores: " << std::endl;
+   std::cin >> valor1 >> valor2;
+    std::string result_of_valor1_valor2{0};
+   std::cout << valor1 << " + " << valor2 << " = " << valor1 + valor2 << std::endl;
+
+   std::cout << valor1 << " - " << valor2 << " = " << valor1 - valor2 << std::endl;
+
+   std::cout << valor1 << " * " << valor2 << " = " << valor1 * valor2 << std::endl;
+
+   std::cout << valor1 << " / " << valor2 << " = " << valor1 / valor2 << std::endl;
+
+   std::cout << valor1 << " % " << valor2 << " = " << valor1 % valor2 << std::endl;
+
+   comprobacion(valor1, valor2, result_of_valor1_valor2);
+
+   std::cout << valor1 << " == " << valor2 << " = " << result_of_valor1_valor2 << std::endl;
+   return 0;
+}
+
+
