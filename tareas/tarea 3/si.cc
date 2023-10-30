36a50e9f494eaaec1683938283d2314d2b804e91
dario <alu0101640150@ull.edu.es>
2023-10-30 16:23:21 +0000

    todo

--- tareas/tarea 3/si.cc
+++ tareas/tarea 3/si.cc
@@ -1,3 +1,17 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2023-2024
+  *
+  * @file 
+  * @author Dario Regalado Gonzalez alu0101640150@ull.edu.es
+  * @date 
+  * @brief 
+  * @bug There are no known bugs
+  * @see https://jutge.org/problems/P48107
+  */
+
 #include <iostream>
 
 int generateTRE (int N){
