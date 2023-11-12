c6e523efd8a8446826a4e08f7038d5e09630b959
dario <alu0101640150@ull.edu.es>
2023-11-12 19:18:50 +0000

    practica09

---
+++ practicas/practica09/practica09-exercises/scalar_product/funciones.h
@@ -0,0 +1,15 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2023-2024
+  *
+  * @author NAME EMAIL
+  * @date Nov 12 2023
+  * @brief DESCRIPTION
+  * @bug There are no known bugs
+  * @see
+  */
+
+#pragma once
+
