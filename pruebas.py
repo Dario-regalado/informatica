e24f233e06ed49af34ae0351adb5186e971d1640
Darío <alu0101640150@ull.edu.es>
2023-12-20 20:31:33 +0000

    "si"

---
+++ pruebas.py
@@ -0,0 +1,9 @@
+import random
+from random import randrange
+
+print("primera hoja:")
+print(random.randint(1,49))
+print("segunda hoja")
+print(random.randint(1, 15))
+print("tercera hoja")
+print(random.randint(1, 30))
