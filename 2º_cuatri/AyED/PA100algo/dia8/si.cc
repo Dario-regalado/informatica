39917e0b5001fb148aadd75f5cfb9ff94bd07bc8
Darío <alu0101640150@ull.edu.es>
2024-04-26 11:26:12 +0100

    dia 9

--- 2º_cuatri/AyED/PA100algo/dia8/si.cc
+++ 2º_cuatri/AyED/PA100algo/dia8/si.cc
@@ -9,6 +9,7 @@
  * 
  */
 
+#include <vector>
 #include <iostream>
 
 int dig(int number) {
@@ -31,12 +32,41 @@ long long int collatz(int number) {
   else return number % 2 == 0 ? 1 + collatz(number / 2) : 1 + collatz(3 + number + 1);
 }
 
+void GeneraBinario(std::vector<char>& v, const int n){
+  if (n == 0) {
+    for (int i = v.size(); i >= 0; i--) {
+      std::cout << v[i] << " ";
+    }
+    std::cout << std::endl; 
+  } 
+  else {
+    v[n-1] = '0';
+    GeneraBinario(v, n-1);
+    v[n-1] = '1';
+    GeneraBinario(v, n-1);
+  }
+}
+
+bool Is_PRime(const int n, int divisor) {
+  if (n == divisor) return true;
+  return (n % divisor == 0) ? false : Is_PRime(n, divisor+1);
+}
+
+bool IsPRime(const int n) { 
+  return Is_PRime(n, 1);
+}
+
 int main() {
-  int number{19700};
-  std::cin >> number;
+  int number{1970};
+  //std::cin >> number;
   std::cout << "el numero de digitos de " << number << " es: " << dig(number) << std::endl;
   std::cout << "la suma de los digitos de " << number << " es: " << sum(number) << std::endl;
   std::cout << "el numero binario de " << number << " es: " << bin(number) << std::endl;
-  std::cout << "la secuencia de collatz de " << number << " es: " << collatz(number) << std::endl;
+  //std::cout << "la secuencia de collatz de " << number << " es: " << collatz(number) << std::endl;
+
+  std::vector<char> numero(5);
+  std::cout << std::endl; GeneraBinario(numero, 3);
+
+  std::cout << number << ((IsPRime(number)) ? " no es primo" : " es primo") << std::endl;
   return 0;
 }
