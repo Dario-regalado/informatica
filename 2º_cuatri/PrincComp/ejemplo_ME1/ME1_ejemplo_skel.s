87dd64d9235426fd4d56da04096a67580df3574e
Darío <alu0101640150@ull.edu.es>
2024-04-29 20:19:50 +0100

    ME1 ejemplo hecho

--- 2º_cuatri/PrincComp/ejemplo_ME1/ME1_ejemplo_skel.s
+++ 2º_cuatri/PrincComp/ejemplo_ME1/ME1_ejemplo_skel.s
@@ -50,10 +50,14 @@ printvec:
 for_printvec:
     bge $s2, $s1, for_printvec_fin
 #         std::cout << v[i] << " ";
-    swc1 $f12, 0($s0)
-    li $v0, 7
+    l.d $f12, 0($s0)
+    li $v0, 3
     syscall
-    addi $s0, sizeD
+    li $v0, 11
+    li $a0, ' '
+    syscall
+
+    addi $s0, $s0, sizeD
 
     addi $s2, 1
     b for_printvec
@@ -98,18 +102,18 @@ while_ordenado:
     bge $t3, $t1, while_ordenado_fin
 if_ordenado:
 #         if (v[i+1] >= v[i]) {
-    addi $t4, $t3, 1
+    li $t4, 1
+    add $t4, $t3, $t4
     mul $t4, $t4, sizeD
-    addi $t4, $t4, $t0
+    add $t4, $t4, $t0
     l.d $f4, 0($t4)
 
-    li $t4, $t3
-    mul $t4, $t4, sizeD
-    addi $t4, $t4, $t0
+    mul $t4, $t3, sizeD
+    add $t4, $t4, $t0
     l.d $f6, 0($t4)
 
     c.lt.d $f4, $f6
-    bc1f if_ordenado_fin
+    bc1t if_ordenado_fin
 
 #             resultado = 0;
     li $t2, 0
@@ -132,50 +136,227 @@ while_ordenado_fin:
 
 
 # void merge(double *v1, const int n1,double *v2, const int n2) {
+merge:
+    addi $sp, -36
+    sw $s0, 0($sp)
+    sw $s1, 4($sp)
+    sw $s2, 8($sp)
+    sw $s3, 12($sp)
+    sw $s4, 16($sp)
+    sw $s5, 20($sp)
+    sw $s6, 24($sp)
+    sw $s7, 28($sp)
+    sw $ra, 32($sp)
+
+# s0-> o1
+# s1-> o2
+# s2-> v1
+# s3-> n1
+# s4-> v2
+# s5-> n2
+# s6 -> i
+# s7 -> j
+    move $s2, $a0
+    move $s3, $a1
+    move $s4, $a2
+    move $s5, $a3
+
 #     int  o1 = ordenado(v1,n1);
 #     if (o1 == 0) return;
+    jal ordenado
+    move $s0, $v0
+    beq $s0, 0, merge_fin
+
 #     int o2 = ordenado(v2,n2);
 #     if (o2 == 0) return;
-#     int i = 0; // índice para recorrer el v1
-#     int j = 0; // índice para recorrer el v2
+    move $a0, $s4
+    move $a1, $s5
+    jal ordenado
+    move $s1, $v0
+    beq $s1, 0, merge_fin
+
+    li $s6, 0 #     int i = 0; // índice para recorrer el v1
+    li $s7, 0 #     int j = 0; // índice para recorrer el v2
 #     while ( ( i < n1) && (j < n2) ) {
+while1_merge:
+    bge $s6, $s3, while1_merge_fin
+    bge $s7, $s5, while1_merge_fin
+
+    mul $t0, $s6, sizeD
+    add $t0, $t0, $s2
+    l.d $f4, 0($t0)
+    mul $t1, $s7, sizeD
+    add $t1, $t1, $s4
+    l.d $f6, 0($t1)
 #         if (v1[i] >= v2[j]) {
+    c.lt.d $f4, $f6
+    bc1f else
+
 #             std::cout << v1[i] << " ";
+    li $v0, 3
+    mov.d $f12, $f4
+    syscall
+    li $v0, 11
+    li $a0, ' '
+    syscall
 #             i++;
+    addi $s6, 1
+    b while1_merge
 #         }
 #         else {
+else:
 #             std::cout << v2[j] << " ";
+    li $v0, 3
+    mov.d $f12, $f6
+    syscall
+    li $v0, 11
+    li $a0, ' '
+    syscall
 #             j++;
+    addi $s7, 1
 #         }
 #     }
+    b while1_merge
+while1_merge_fin:
 #     while ( i < n1) {
+while2_merge:
+    bge $s6, $s3, while2_merge_fin
+    mul $t0, $s6, sizeD
+    add $t0, $s2, $t0
 #         std::cout << v1[i] << " ";
+    li $v0, 3
+    l.d $f12, 0($t0)
+    syscall
+    li $v0, 11
+    li $a0, ' '
+    syscall
 #         i++;
+    addi $s6, 1
 #     }
+    b while2_merge
+while2_merge_fin:
+
 #     while ( j < n2) {
+while3_merge:
+    bge $s7, $s5, while3_merge_fin
+
 #         std::cout << v2[j] << " ";
+    mul $t1, $s7, sizeD
+    add $t1, $t1, $s4
+    li $v0, 3
+    l.d $f12, 0($t1)
+    syscall
+    li $v0, 11
+    li $a0, ' '
+    syscall
 #         j++;
+    addi $s7, 1 
 #     }
+    b while3_merge
+while3_merge_fin:
 #     std::cout << "\n";
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+
+merge_fin:
+    lw $s0, 0($sp)
+    lw $s1, 4($sp)
+    lw $s2, 8($sp)
+    lw $s3, 12($sp)
+    lw $s4, 16($sp)
+    lw $s5, 20($sp)
+    lw $s6, 24($sp)
+    lw $s7, 28($sp)
+    lw $ra, 32($sp)
+    addi $sp, 36
+    jr $ra
 #     return;
 # }
 
+
+
 # int main(void) {
+main:
+
 #     std::cout << "\nVector con dimension " << n1 << std::endl;
+    li $v0, 4
+    la $a0, cad1
+    syscall
+    li $v0, 1
+    lw $a0, n1
+    syscall 
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+
 #     printvec(v1,n1);
+    la $a0, v1
+    lw $a1, n1
+    jal printvec
   
 #     std::cout << "\nVector con dimension " << n2 << std::endl;
+    li $v0, 4
+    la $a0, cad1
+    syscall
+    li $v0, 1
+    lw $a0, n2
+    syscall
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+
+
 #     printvec(v2,n2);
+    la $a0, v2
+    lw $a1, n2
+    jal printvec
   
 #     std::cout << "\nVector con dimension " << n3 << std::endl;
+    li $v0, 4
+    la $a0, cad1
+    syscall
+    li $v0, 1
+    lw $a0, n3
+    syscall
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+
 #     printvec(v3,n3);
+    la $a0, v3
+    lw $a1, n3
+    jal printvec
   
 #     std::cout << "\nIntentando mezcla con dos vectores ...\n";
+    li $v0, 4
+    la $a0, cad2
+    syscall
+
 #     merge(v1,n1,v2,n2);
+    la $a0, v1
+    lw $a1, n1
+    la $a2, v2
+    lw $a3, n2
+    jal merge
+
 #     std::cout << "\nIntentando mezcla con dos vectores ...\n";
+    li $v0, 4
+    la $a0, cad2
+    syscall
 #     merge(v1,n1,v3,n3);
+    la $a0, v1
+    lw $a1, n1
+    la $a2, v3
+    lw $a3, n3
+    jal merge
 
 #     std::cout << "\nFIN DEL PROGRAMA\n";
+    li $v0, 4
+    la $a0, cad3
+    syscall
+    li $v0, 10
+    syscall
 #     return(0);
 # }
 
