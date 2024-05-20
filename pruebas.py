512c440d7ca0b57aa4208d7b20514f5575fcdf98
Darío <alu0101640150@ull.edu.es>
2024-05-20 11:42:44 +0100

    si

--- pruebas.py
+++ pruebas.py
@@ -2,8 +2,8 @@ import random
 from random import randrange
 
 print("primera hoja:")
-print(random.randint(1,49))
-print("segunda hoja")
-print(random.randint(1, 15))
-print("tercera hoja")
-print(random.randint(1, 30))
+print(random.randint(20,39))
+#print("segunda hoja")
+#print(random.randint(1, 15))
+#print("tercera hoja")
+#print(random.randint(1, 30))
