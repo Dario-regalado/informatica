7de6c8fcf2f6a67c39735ba6720ce5224351cba4
Dario Regalado <alu0101640150@ull.edu.es>
2024-06-04 00:22:38 +0100

    si

--- si.cc
+++ si.cc
@@ -1,21 +1,50 @@
 #include <iostream>
-int grid[3][3] = {{1, 2, 3},
-                  {4, 5, 6},
-                  {7, 8, 9}};
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main() {
-  while (true){
-
-    for (int row = 0; row < 3; row++)
-    {
-      for (int col = 0; col < 3; col++)
-      {
-        std::cout << grid[row][col];
-      }
-      std::cout << std::endl;
+void suma (ifstream& in1, ifstream& in2, const string& nombre) {
+    ofstream out1(nombre);
+    if (!in1.is_open() && !in2.is_open())
+        cerr << "puta"; 
+    string a;
+    string b;
+    while(in1 && in2) {
+        in1 >> a;
+        in2 >> b;
+        out1 << a << " " << b << " ";
+        a.clear();
+        b.clear();
     }
-    std::cout << std::endl;
-    std::cout << std::endl;
-  }
-  
+    
+    if (in1) {
+        in1 >> a;
+        out1 << a << " ";
+        a.clear();
+    }
+    if (in2) {
+        in2 >> a;
+        out1 << a << " ";
+        a.clear();
+    }
+    
+    in1.close();
+    in2.close();
+    out1.close();
+}
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(const int argc, char* argv[]) {
+    string nombre1;
+    //cin >> nombre1;
+    ifstream fichero1(argv[1]);
+    ifstream fichero2(argv[2]);
+    suma(fichero1, fichero2, nombre1);
+    return 0;
 }
