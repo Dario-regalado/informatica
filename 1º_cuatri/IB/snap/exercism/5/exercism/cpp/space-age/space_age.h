056c58b0f03881459b63975a8d12bf8bbca27ecf
Dario Regalado <alu0101640150@ull.edu.es>
2024-07-18 00:32:09 +0100

    update ib

---
+++ 1º_cuatri/IB/snap/exercism/5/exercism/cpp/space-age/space_age.h
@@ -0,0 +1,24 @@
+#if !defined(SPACE_AGE_H)
+#define SPACE_AGE_H
+
+namespace space_age {
+  class space_age{
+   public:
+    space_age(double seconds) {time_ = seconds;}
+    double seconds() const;
+    double on_mercury() const;
+    double on_venus() const;
+    double on_earth() const;
+    double on_mars() const;
+    double on_jupiter() const;
+    double on_saturn() const;
+    double on_uranus() const;
+    double on_neptune() const;
+
+   private:
+    double time_{};
+    };
+
+}  // namespace space_age
+
+#endif // SPACE_AGE_H
--- si.cc
+++ si.cc
@@ -1,16 +1,25 @@
 #include <array>
 #include <vector>
 #include <iostream>
-
-constexpr int pepe(void) {
-    return 10;
-}
-
-int main() {
-    std::array<int, 10> pepi;
-    std::vector<int> pepa;
-    const int i = pepe();
-    std::cin >> i;
-    constexpr int j = 10;
-    constexpr int k = pepe();
+#include <fstream>
+class Racional {
+    Racional() = default;
+    friend std::ifstream& operator>>(std::ifstream& is, Racional& racional);
+    private:
+    int numerador_, denominador_;
+};
+std::ifstream& operator>>(std::ifstream& is, Racional& racional) {
+    auto temp{0};
+    bool count{false};
+    while(is >> temp) {
+        if(isdigit(temp)){
+            if(count == false){
+                racional.numerador_ = temp;
+                count = true;
+            } else {
+                racional.denominador_ = temp;
+                break;
+            }
+        }
+    }
 }
