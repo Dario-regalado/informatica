a9960b3e7817a08e09d5d282d6d9b32dab275984
Darío <alu0101640150@ull.edu.es>
2024-09-23 22:57:49 +0100

    proyect maker v1.1

--- proyect_maker/src/main.cc
+++ proyect_maker/src/main.cc
@@ -20,7 +20,7 @@ using namespace std;
 void Usage(const int argc, char* argv[]);
 void CreateClass(const int n, char* argv[], const string&);
 void CreateImpl(const int n, char* argv[], const string&);
-void CreateMain(char* argv[], string);
+void CreateMain(char* argv[], const string&);
 
 
 int main(const int argc, char* argv[]) {
@@ -57,27 +57,36 @@ void CreateClass(const int n, char* argv[], const string& directory) {
     
     string line;
     while (getline(input_file, line)) {
-      string words;
-      istringstream input_stream(line);
-      while (input_stream >> words) {
-        if (words == "(reemplace)") {
-          words = argv[i];
-          words[0] = toupper(words[0]);  // Pone la primera letra en mayúscula
+      size_t pos = 0;
 
-          // Reemplaza los guiones bajos por mayúsculas
-          for (size_t j = 0; j < words.size(); j++) {
-            if (words[j] == '_') {
-              words.erase(j, 1);  // Borra el guion bajo
-              words[j] = toupper(words[j]);  // Pone en mayúscula la letra siguiente
-              j--;  // Ajustamos el índice para no saltar caracteres
-            }
+      // Reemplazamos todas las ocurrencias de "(reemplace)" por argv[i]
+      while ((pos = line.find("(reemplace)", pos)) != string::npos) {
+        string class_name = argv[i];
+        class_name[0] = toupper(class_name[0]);  // Pone la primera letra en mayúscula
+
+        // Reemplaza guiones bajos por mayúsculas
+        for (size_t j = 0; j < class_name.size(); j++) {
+          if (class_name[j] == '_') {
+            class_name.erase(j, 1);  // Borra el guion bajo
+            class_name[j] = toupper(class_name[j]);  // Pone en mayúscula la letra siguiente
+            j--;  // Ajusta el índice
           }
-        } else if (words == "{reemplace}") {
-          words = argv[1];  // Reemplaza el nombre del archivo
         }
-        output_files << words << ' ';  // Escribe la palabra con un espacio
+
+        // Reemplaza "(reemplace)" por el nombre de la clase formateado
+        line.replace(pos, 11, class_name);
+        pos += class_name.size();  // Avanza el índice después del reemplazo
       }
-      output_files << '\n';  // Agrega salto de línea después de cada línea del archivo de entrada
+
+      // Reemplazamos todas las ocurrencias de "{reemplace}" por argv[1] (nombre del archivo)
+      pos = 0;
+      while ((pos = line.find("{reemplace}", pos)) != string::npos) {
+        line.replace(pos, 11, argv[1]);
+        pos += string(argv[1]).size();  // Avanza el índice después del reemplazo
+      }
+
+      // Escribimos la línea completa, conservando los espacios iniciales
+      output_files << line << '\n';
     }
     
     output_files.close();
@@ -85,13 +94,14 @@ void CreateClass(const int n, char* argv[], const string& directory) {
   }
 }
 
+
 /**
  * @brief Create a Impl file
  * 
  * @param n 
  * @param argv 
  */
-void CreateImpl(const int n, char* argv[], const string& directory){
+void CreateImpl(const int n, char* argv[], const string& directory) {
   ofstream output_files;
   ifstream input_file;
   
@@ -107,27 +117,36 @@ void CreateImpl(const int n, char* argv[], const string& directory){
     
     string line;
     while (getline(input_file, line)) {
-      string words;
-      istringstream input_stream(line);
-      while (input_stream >> words) {
-        if (words == "(reemplace)") {
-          words = argv[i];
-          words[0] = toupper(words[0]);  // Pone la primera letra en mayúscula
+      size_t pos = 0;
 
-          // Reemplaza los guiones bajos por mayúsculas
-          for (size_t j = 0; j < words.size(); j++) {
-            if (words[j] == '_') {
-              words.erase(j, 1);  // Borra el guion bajo
-              words[j] = toupper(words[j]);  // Pone en mayúscula la letra siguiente
-              j--;  // Ajustamos el índice para no saltar caracteres
-            }
+      // Reemplazamos todas las ocurrencias de "(reemplace)" por argv[i]
+      while ((pos = line.find("(reemplace)", pos)) != string::npos) {
+        string class_name = argv[i];
+        class_name[0] = toupper(class_name[0]);  // Pone la primera letra en mayúscula
+
+        // Reemplaza guiones bajos por mayúsculas
+        for (size_t j = 0; j < class_name.size(); j++) {
+          if (class_name[j] == '_') {
+            class_name.erase(j, 1);  // Borra el guion bajo
+            class_name[j] = toupper(class_name[j]);  // Pone en mayúscula la letra siguiente
+            j--;  // Ajusta el índice
           }
-        } else if (words == "{reemplace}") {
-          words = argv[1];  // Reemplaza el nombre del archivo
         }
-        output_files << words << ' ';  // Escribe la palabra con un espacio
+
+        // Reemplaza "(reemplace)" por el nombre de la clase formateado
+        line.replace(pos, 11, class_name);
+        pos += class_name.size();  // Avanza el índice después del reemplazo
       }
-      output_files << '\n';  // Agrega salto de línea después de cada línea del archivo de entrada
+
+      // Reemplazamos todas las ocurrencias de "{reemplace}" por argv[1] (nombre del archivo)
+      pos = 0;
+      while ((pos = line.find("{reemplace}", pos)) != string::npos) {
+        line.replace(pos, 11, argv[1]);
+        pos += string(argv[1]).size();  // Avanza el índice después del reemplazo
+      }
+
+      // Escribimos la línea completa, conservando los espacios iniciales
+      output_files << line << '\n';
     }
     
     output_files.close();
@@ -135,32 +154,44 @@ void CreateImpl(const int n, char* argv[], const string& directory){
   }
 }
 
+
 /**
  * @brief Create a Main file
  * 
  * @param argv 
  */
-void CreateMain(char* argv[], string directory){
+void CreateMain(char* argv[], const string& directory) {
   ofstream output_files;
   ifstream input_file;
+  
+  // Abrimos los archivos de entrada y salida
   input_file.open("main_file.txt", std::ios::in);
-  output_files.open(directory + '/' + argv[1] + string(".cc"), std::ios::out);;
+  output_files.open(directory + '/' + argv[1] + ".cc", std::ios::out);
+  
+  if (!input_file.is_open() || !output_files.is_open()) {
+    cerr << "Error al abrir los archivos." << endl;
+    return;
+  }
   
   string line;
+  
+  // Leemos el archivo línea por línea
   while (getline(input_file, line)) {
-    string word;
-    istringstream line_stream(line);  // Convertimos la línea en un stream para leer palabra por palabra
+    size_t pos = 0;
 
-    while (line_stream >> word) {
-      if (word == "{reemplace}") {
-        word = argv[1];  // Reemplazamos la palabra por el nombre del archivo
-      }
-      output_files << word << " ";
+    // Reemplazamos todas las ocurrencias de "{reemplace}" por argv[1]
+    while ((pos = line.find("{reemplace}", pos)) != string::npos) {
+      line.replace(pos, 11, argv[1]);  // 11 es la longitud de "{reemplace}"
+      pos += string(argv[1]).size();  // Avanzamos el índice después del reemplazo
     }
-
-    // Agregamos un salto de línea al final de cada línea procesada
-    output_files << std::endl;
+    
+    // Escribimos la línea completa tal como es, incluyendo los espacios al inicio
+    output_files << line << endl;
   }
+  
+  // Cerramos los archivos
+  input_file.close();
+  output_files.close();
 }
 
 /** @brief Shows correct usage of the program. 
