4d242bd2a93b7fb6b937db630950714e42c94dfd
dario <alu0101640150@ull.edu.es>
2024-09-30 08:36:10 +0000

    p01 EC

--- 2º/1º_cuatri/EC/p01/practica/fa_vr.v
+++ 2º/1º_cuatri/EC/p01/practica/fa_vr.v
@@ -1,5 +1,5 @@
 //Sumador completo (full-adder) de tres entradas de 1 bit realizado a partir de puertas l�gicas 
-module fa_v1(output wire sum, output wire c_out, input wire a, input wire b, input wire c_in);
+module fa_vr(output wire sum, output wire c_out, input wire a, input wire b, input wire c_in);
 
 //Declaraci�n de conexiones internas:
 wire sum1;
@@ -7,8 +7,8 @@ wire carry1;
 wire carry2;
 //Estructura interna: Instancias de puertas y sus conexiones
 
-ha_vr ha1(sum1, carry1, a, b);
-ha_vr ha2(sum, carry2, c_in, sum1);
+ha_vr har1(sum1, carry1, a, b);
+ha_vr har2(sum, carry2, c_in, sum1);
 or or1(c_out, carry1, carry2);
 
 endmodule
