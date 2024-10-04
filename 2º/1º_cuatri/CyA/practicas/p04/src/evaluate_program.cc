779daa7daa8225aff89aba82dec8c57a9c11127d
Darío <alu0101640150@ull.edu.es>
2024-10-04 01:07:52 +0100

    p04

--- 2º/1º_cuatri/CyA/practicas/p04/src/evaluate_program.cc
+++ 2º/1º_cuatri/CyA/practicas/p04/src/evaluate_program.cc
@@ -15,32 +15,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <regex>
 #include "variables.h"
 #include "statements.h"
 #include "comments.h"
 
+
+void WriteDescricion(std::ifstream&, std::ofstream&);
 void Usage(const int argc, char* argv[]);
-void EvaluateProgram(std::ifstream&, std::ofstream&);
+void EvaluateProgram(std::ifstream&, std::ofstream&, char* argv[]);
 
 int main(const int argc, char* argv[]) {
   Usage(argc, argv);
   std::ifstream input_file;
   std::ofstream output_file;
   input_file.open(argv[1], std::ios::in);
-  output_file.open(argv[2], std::ios::out); //escribe sin sobreescribir datos
+  output_file.open(argv[2], std::ios::out); 
   if(!input_file.is_open() || !output_file.is_open())
     std::cout <<"no se abrieron los archivos\n";
   
-  EvaluateProgram(input_file, output_file);
+  EvaluateProgram(input_file, output_file, argv);
   return 0;
 }
 
 
-void EvaluateProgram(std::ifstream& input_stream, std::ofstream& output_stream){
-  Comments var;
+void EvaluateProgram(std::ifstream& input_stream, std::ofstream& output_stream, char* argv[]){
+  Variables var;
   var.EvaluateFile(input_stream);
-  output_stream << var;
-  std::cout << var << std::endl;
+  Comments comments;
+  comments.EvaluateFile(input_stream);
+  //ºStatements estatutos;
+  //ºestatutos.EvaluateFile(input_stream);
+
+  output_stream << "PROGRAM: " << argv[1] << std::endl;
+  WriteDescricion(input_stream, output_stream);
+  output_stream << var << std::endl;
+  //output_stream << estatutos << std::endl;
+  output_stream << comments << std::endl;
+}
+
+// ^(\/\*)[\s\S]*?(\*\/)$ para el comentario en bloque del inicio
+void WriteDescricion(std::ifstream& file_input, std::ofstream& output_stream) {
+  //carga el archivo en un string
+  std::stringstream buffer_in;
+  buffer_in << file_input.rdbuf();
+  std::string file_content = buffer_in.str();
+
+  std::regex description_comment(R"(\/\*\*[\s\S]*?\*\/)", std::regex::ECMAScript);
+  std::smatch matches;
+  std::string description;
+  if(std::regex_search(file_content, matches, description_comment, std::regex_constants::match_continuous)) {
+    std::cout << "match";
+    description = matches.str();
+  }
+  output_stream << "DESCRIPTION:\n" << description;
 }
 
 
