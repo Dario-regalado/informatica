fe6bce7e20a0a4a57432ab192f9102ecc7381cb2
Dario Regalado <alu0101640150@ull.edu.es>
2024-10-06 19:12:13 +0100

    p04 arreglos

--- 2º/1º_cuatri/CyA/practicas/p04/p04_casa/program.cc
+++ 2º/1º_cuatri/CyA/practicas/p04/p04_casa/program.cc
@@ -8,23 +8,9 @@
 8 *
 9 * @see https :// github .com/IB -2023 -2024/ IB -class -code - examples /
  */
-
 #include <iostream>
 #include <cassert>
-// Returns the factorial of the argument
-int Factorial (int number ) {
-  switch ( number ) {
-    case 0:
-    case 1:
-      return 1;
-    default :
-      int factorial = 1;
-      for (int i = 1; i <= number ; ++ i ) {
-        factorial *= i ;
-      }
-      return factorial ;
-  }
-}
+int Factorial(int number);
 /**
  * @brief 
  * 
@@ -37,5 +23,23 @@ int main () {
   for (int i = 1; i <= limit ; ++ i ) {
     std::cout << i << "! = " << (double) Factorial ( i ) << std :: endl ;
   }
+  while(true) {
+    std::cout << "hola\n";
+  }
   return 0;
+}
+// Returns the factorial of the argument
+int Factorial (int number ) {
+  switch ( number ) {
+    case 0:
+    case 1:
+      return 1;
+    default :
+      int factorial = 1;
+      for (int i = 1; i <= number ; ++ i ) {
+        factorial *= i ;
+      }
+      return factorial ;
+      double u = 3.44;
+  }
 }
